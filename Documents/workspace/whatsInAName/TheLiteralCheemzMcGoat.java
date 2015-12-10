32bf7ebed358506ecea9071c5eaa0a0c2b726e78
Student <student@c02kw1wjfh51.dalton.org>
2015-12-10 09:44:30 -0500

    second

--- Documents/workspace/whatsInAName/TheLiteralCheemzMcGoat.java
+++ Documents/workspace/whatsInAName/TheLiteralCheemzMcGoat.java
@@ -1,4 +1,63 @@
 
 public class TheLiteralCheemzMcGoat {
-
+	static char[] reverse 
+	{
+		return
+	}
+	static int vowel
+	{
+		
+	}
+	static int consonentFrequency
+	{
+		
+	}
+	static String firtsName
+	{
+		
+	}
+	static string lastName
+	{
+		
+	}
+	static string middleName
+	{
+		
+	}
+	static boolean hyphen
+	{
+		
+	}
+	static char[] lowerCase
+	{
+		
+	}
+	static char[] upperCase
+	{
+		
+	}
+	static char[] random
+	{
+		
+	}
+	static boolean palindrome
+	{
+		
+	}
+	static char[] sorted
+	{
+		
+	}
+	static char[] initials
+	{
+		
+	}
+	static boolean title
+	{
+		
+	}
+	static char[] strongSyllable
+	{
+		
+	}
 }
